aae340739e99b68160a60354083d52a63c3628d0
Andres Benavides <dandavi2310@hotmail.com>
2025-02-04 13:02:32 -0500

    update the lambda handler

--- main.tf
+++ main.tf
@@ -1,31 +1,15 @@
 provider "aws" {
   region = "us-east-1"
 }
+
 ##############################
-# 2. Bucket S3 para el ZIP de Lambda
+# Bucket S3 para el ZIP de Lambda
 ##############################
 resource "aws_s3_bucket" "lambda_bucket_poc" {
   bucket        = "my-lambda-report-bucket-poc"
   force_destroy = true
 }
 
-# resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
-#   bucket = aws_s3_bucket.lambda_bucket_poc.id
-#   policy = jsonencode({
-#     Version   = "2012-10-17",
-#     Statement = [
-#       {
-#         Sid       = "AllowLambdaGetObject",
-#         Effect    = "Allow",
-#         Principal = "*",
-#         Action    = "s3:GetObject",
-#         Resource  = "${aws_s3_bucket.lambda_bucket_poc.arn}/*"
-#       }
-#     ]
-#   })
-# }
-
-# Opción A: Desactivar bloqueo de políticas públicas para poder aplicar la política
 resource "aws_s3_bucket_public_access_block" "lambda_bucket_access" {
   bucket                  = aws_s3_bucket.lambda_bucket_poc.id
   block_public_acls       = false
@@ -34,7 +18,9 @@ resource "aws_s3_bucket_public_access_block" "lambda_bucket_access" {
   restrict_public_buckets = false
 }
 
+##############################
 # DynamoDB
+##############################
 resource "aws_dynamodb_table" "incidencias" {
   name           = "IncidenciasMantenimiento"
   billing_mode   = "PAY_PER_REQUEST"
@@ -44,9 +30,18 @@ resource "aws_dynamodb_table" "incidencias" {
     name = "incidenciaId"
     type = "S"
   }
+  global_secondary_index {
+    name               = "DispositivoIdIndex"
+    hash_key           = "dispositivoId"
+    projection_type    = "ALL"
+    write_capacity     = 1
+    read_capacity      = 1
+  }
 }
 
+##############################
 # IAM Role para Lambda
+##############################
 resource "aws_iam_role" "lambda_role" {
   name = "lambda_iot_dynamodb_role"
   assume_role_policy = jsonencode({
@@ -63,7 +58,6 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-# Permisos de Lambda para DynamoDB
 resource "aws_iam_policy" "dynamodb_policy" {
   name   = "LambdaDynamoDBAccessPolicy"
   policy = jsonencode({
@@ -78,7 +72,8 @@ resource "aws_iam_policy" "dynamodb_policy" {
         Effect = "Allow",
         Action = ["logs:CreateLogGroup", "logs:CreateLogStream", "logs:PutLogEvents"],
         Resource = "*"
-      },{
+      },
+      {
         Effect = "Allow",
         Action = ["lambda:UpdateFunctionCode"],
         Resource = aws_lambda_function.iot_lambda.arn
@@ -92,18 +87,21 @@ resource "aws_iam_policy" "dynamodb_policy" {
   })
 }
 
-# Asignar la política al rol de Lambda
 resource "aws_iam_role_policy_attachment" "lambda_dynamodb_attach" {
   role       = aws_iam_role.lambda_role.name
   policy_arn = aws_iam_policy.dynamodb_policy.arn
 }
 
+##############################
 # Lambda desde S3
+##############################
 resource "aws_lambda_function" "iot_lambda" {
   function_name = "TemperatureAlertHandler"
   role          = aws_iam_role.lambda_role.arn
   handler       = "com.iotPoc.TemperatureAlertHandler::handleRequest"
   runtime       = "java17"
+  memory_size   = 512
+  timeout       = 10
 
   s3_bucket = aws_s3_bucket.lambda_bucket_poc.id
   s3_key    = var.lambda_s3_key
@@ -115,7 +113,9 @@ resource "aws_lambda_function" "iot_lambda" {
   }
 }
 
+##############################
 # Regla de IoT Core
+##############################
 resource "aws_iot_topic_rule" "iot_rule" {
   name        = "HighTemperatureRule"
   sql         = "SELECT * FROM 'data' WHERE Temperatura > 49"
@@ -127,7 +127,6 @@ resource "aws_iot_topic_rule" "iot_rule" {
   }
 }
 
-# Permiso para que IoT invoque Lambda
 resource "aws_lambda_permission" "allow_iot" {
   statement_id  = "AllowIoTInvokeLambda"
   action        = "lambda:InvokeFunction"
